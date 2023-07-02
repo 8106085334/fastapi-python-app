69c89b2c97f7f17024944a0443088746a87f8a15
vinayb11 <vinayb11@gmail.com>
2023-07-02 18:18:05 +0530

    Add update and delete operation API endpoints

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from typing import List
 from uuid import uuid4
 from fastapi import FastAPI
 from models import Gender, Role, User
+from uuid import UUID
+from fastapi import HTTPException
 
 app = FastAPI()
 
@@ -17,6 +19,28 @@ async def get_users():
 async def create_user(user: User):
   db.append(user)
   return {"id": user.id}
+
+@app.delete("/api/v1/users/{id}")
+async def delete_user(id: UUID):
+  for user in db:
+    if user.id == id:
+      db.remove(user)
+      return
+  raise HTTPException(
+    status_code=404, detail=f"Delete user failed, id {id} not found."
+  )
+@app.put("/api/v1/users/{id}")
+async def update_user(user_update: User, id: UUID):
+  for user in db:
+    if user.id == id:
+      if user_update.first_name is not None:
+        user.first_name = user_update.first_name
+        if user_update.last_name is not None:
+          user.last_name = user_update.last_name
+          if user_update.roles is not None:
+            user.roles = user_update.roles
+            return user.id
+            raise HTTPException(status_code=404, detail=f"Could not find user with id: {id}")
 
 db: List[User] = [
          User(
